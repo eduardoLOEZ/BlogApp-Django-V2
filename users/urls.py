d12ac73131ca275af81c542f0c8beb6a86ce9c54
Eduardo Loeza <84247208+eduardoLOEZ@users.noreply.github.com>
2023-08-03 18:06:50 -0600

    Add files via upload

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 
@@ -12,3 +14,5 @@ urlpatterns = [
     path('getUsers/', views.getUsers),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
